3fa19fd8c3ac6e7ad5da2eedc55a0060894fb6b5
geraldo ferreira <geraldosousadev@gmail.com>
2025-01-27 15:51:05 -0300

    getting title and price data from the page

--- app.js
+++ app.js
@@ -8,7 +8,23 @@ async function start() {
     const page = await browser.newPage();
     await page.goto(url);
 
+    let posts = await page.evaluate(()=>{
+        let items = [];
+        document.querySelectorAll('li > article > h3 > a').forEach((item)=>{
+            let post = {'titulo' : item.title};
+            items.push(post);
+        });
+        document.querySelectorAll('.price_color').forEach((item, i)=>{
+            items[i].preco = item.textContent
+            // items.push({preço: item.textContent});
+        });
+
+        return items;
+    });
+
+    console.log(posts);
+
     await browser.close();
-}
+};
 
 start();
